75fd1a5857ab43225a2b73b81cf46d6c3f46075a
nikikiarie <nix.kiariebiz@gmail.com>
2024-08-18 17:44:18 +0300

    await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let obj = {};
+
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    obj = { photo, user };
+  } catch (err) {
+    obj = { photo: null, user: null };
+  }
+  return obj;
+}
