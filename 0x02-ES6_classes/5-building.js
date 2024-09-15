0526e11af0db068f6b3312c7ab9478064fd353b9
nikikiarie <nix.kiariebiz@gmail.com>
2024-09-15 16:07:03 +0300

    A Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,17 +2,14 @@ class Building {
   constructor(sqft) {
     if (this.constructor !== Building) {
       if (typeof this.evacuationWarningMessage !== 'function') {
-          throw new Error(
+        throw new Error(
             'Class extending Building must override evacuationWarningMessage',
-          );
+        );
       }
     }
-  this.sqft = sqft;
+    this.sqft = sqft;
   }
-  
-    /**
-     * @param {Number} sqft
-     */
+
   set sqft(sqft) {
     if (typeof sqft !== 'number') {
       throw new TypeError('sqft must be a number');
