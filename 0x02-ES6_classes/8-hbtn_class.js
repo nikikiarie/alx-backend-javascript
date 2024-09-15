84f5e237739a283d2b15b3babc32efa4b0441963
nikikiarie <nix.kiariebiz@gmail.com>
2024-09-15 16:21:31 +0300

    Primitive - Holberton Class

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,10 +1,10 @@
 export default class HolbertonClass {
   constructor(size, location) {
     if (typeof size !== 'number') {
-      throw new TypeError('Size must be a number');
+      throw new TypeError('Size is a number');
     }
     if (typeof location !== 'string') {
-      throw new TypeError('Location must be a string');
+      throw new TypeError('Location is a string');
     }
     this._size = size;
     this._location = location;
