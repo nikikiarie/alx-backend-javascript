39458eff30de6a5a9e5d43488dd24998ec91966e
nikikiarie <nix.kiariebiz@gmail.com>
2024-09-15 13:58:18 +0300

    A Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,21 @@
-export default class Building {
+class Building {
     constructor(sqft) {
       if (this.constructor !== Building) {
-        const props = Object.getOwnPropertyNames(this.constructor.prototype);
-        if (!props.find((err) => err === 'evacuationWarningMessage')) {
-          throw new Error('Class extending Building must override evacuationWarningMessage');
+        if (typeof this.evacuationWarningMessage !== 'function') {
+          throw new Error(
+            'Class extending Building must override evacuationWarningMessage',
+          );
         }
+      }
+      this.sqft = sqft;
+    }
+  
+    /**
+     * @param {Number} sqft
+     */
+    set sqft(sqft) {
+      if (typeof sqft !== 'number') {
+        throw new TypeError('sqft nust be a number');
       }
       this._sqft = sqft;
     }
@@ -12,11 +23,6 @@ export default class Building {
     get sqft() {
       return this._sqft;
     }
-  
-    set sqft(sqft) {
-      if ((typeof sqft !== 'number') && (sqft instanceof Number)) {
-        throw new TypeError('Sqft must be a number');
-      }
-      this._sqft = sqft;
-    }
   }
+  
+  export default Building;
