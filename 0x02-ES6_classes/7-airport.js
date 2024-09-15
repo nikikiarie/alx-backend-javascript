9c6c3d542f813905819bfc815176e009f958f9c2
nikikiarie <nix.kiariebiz@gmail.com>
2024-09-15 16:18:34 +0300

    Airport

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,10 +1,10 @@
 export default class Airport {
   constructor(name, code) {
     if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
+      throw new TypeError('Name should be a string');
     }
     if (typeof code !== 'string') {
-      throw new TypeError('Code must be a string');
+      throw new TypeError('Code should be a string');
     }
     this._name = name;
     this._code = code;
