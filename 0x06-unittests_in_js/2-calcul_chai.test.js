d5d670ee68bf477c8602c4420c0f221a4c2a587c
nikikiarie <nix.kiariebiz@gmail.com>
2024-09-19 22:25:02 +0300

    Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -5,32 +5,32 @@ const calculateNumber = require("./2-calcul_chai");
 describe("calculateNumber", function() {
     describe("SUM", function() {
 	it("test if", function() {
-	    expect(calculateNumber("SUM", 3.2, 5.5)).to.equal(9);
+	    expect(calculateNumber("SUM", 1, 2)).to.equal(3);
 	});
 	it("test if", function() {
-	    expect(calculateNumber("SUM", 2.1, 1.8)).to.equal(4);
+	    expect(calculateNumber("SUM", 1.6, 2.6)).to.equal(5);
 	});
     });
     describe("SUBTRACT", function() {
 	it("test if", function() {
-	    expect(calculateNumber("SUBTRACT", 6.7, 3.4)).to.equal(3);
+	    expect(calculateNumber("SUBTRACT", 1.4, 2.3)).to.equal(-1);
 	});
 	it("test if", function() {
-	    expect(calculateNumber("SUBTRACT", 8.9, 4.2)).to.equal(5);
+	    expect(calculateNumber("SUBTRACT", 4.9, 2.7)).to.equal(2);
 	});
 	it("test if", function() {
-	    expect(calculateNumber("SUBTRACT", -7.8, -2.3)).to.equal(-5);
+	    expect(calculateNumber("SUBTRACT", -4.9, -2.7)).to.equal(-2);
 	});
     });
     describe("DIVIDE", function() {
 	it("test if", function() {
-	    expect(calculateNumber("DIVIDE", 10.4, 2.5)).to.equal(4);
+	    expect(calculateNumber("DIVIDE", 4, 2)).to.equal(2);
 	});
 	it("test if", function() {
-	    expect(calculateNumber("DIVIDE", 5.6, 2.2)).to.equal(3);
+	    expect(calculateNumber("DIVIDE", 4.6, 1.8)).to.equal(2.5);
 	});
 	it("test if", function() {
-	    expect(calculateNumber("DIVIDE", 8, 0)).to.equal("Error");
+	    expect(calculateNumber("DIVIDE", 4, 0)).to.equal("Error");
 	});
     });
 });
